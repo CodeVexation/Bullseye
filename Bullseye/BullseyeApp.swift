8d5496049f4f298ed53599cadc5be55e586246bf
Vivek Pillai <aarryann@yahoo.com>
2024-03-23 18:42:43 -0400

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Vivek Pillai on 3/23/24.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
