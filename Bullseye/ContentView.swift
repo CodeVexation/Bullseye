19bb8160b011e0d06047624155151458455d6dcf
Varun Pillai <yoshibotter@gmail.com>
2024-03-24 17:35:17 -0400

    minor ui changes

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,23 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var alertIsVisible: Bool = false
+    
+    
     var body: some View {
         VStack {
             Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN")
                 .bold()
                 .multilineTextAlignment(.center)
-                .lineSpacing(4.0)
+                .lineSpacing(2.0)
                 .font(.footnote)
                 .kerning(2.0)
             Text("90")
+                .fontWeight(.black)
+                .kerning(-1.0)
+                .font(.largeTitle)
+
             HStack {
                 Text("1")
+                    .bold()
                 Slider(value: .constant(89), in: 1.0...100.0)
                 Text("100")
+                    .bold()
             }
-                Button("Hit Me") {
-                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                    }
+            Button(action: {
+              print("Hello, SwiftUI!")
+                self.alertIsVisible = true
+            }) {
+              Text("Hit me")
+            }
+            alert.(isPresented: $alertIsVisible, content: {
+                return Alert(title: Text("Hello There"), message: Text("This is my first pop up"), dismissButton: .default("Awesome!"))
+            })
                     
                 }
             }
@@ -34,3 +50,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
